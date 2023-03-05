a25c8f2ab792ff26860129d386385c4f726d1bbe
Andee Warren <119145298+andeewarren@users.noreply.github.com>
2023-03-05 14:17:20 -0500

    style update

--- script.js
+++ script.js
@@ -27,8 +27,8 @@ let cells = document.getElementsByClassName('box')
 let startButton = document.getElementById('start')
 
 status.innerHTML = `Press start to begin`;
-// player1Score.innerHTML = 0;
-// player2Score.innerHTML = 0;
+player1Score.innerHTML = 0;
+player2Score.innerHTML = 0;
 
 //make start button clickable
 startButton.addEventListener('click', () => {
@@ -44,11 +44,11 @@ document.getElementById('reset').addEventListener('click', () => {
     gameActive = false;
     turns = 0;
     gameState = ["", "", "", "", "", "", "", "", ""];
-    // for (cell of cells){
-    //     cell.innerHTML = "";
-    // };
-    location.reload();
-    // status.innerHTML = `Game board successfully reset`
+    for (cell of cells){
+        cell.innerHTML = "";
+    };
+    // location.reload();
+    status.innerHTML = `Game board successfully reset`
     // console.log(gameActive)
     // console.log(gameState)
 })
@@ -60,12 +60,12 @@ function checkWin () {
             if (gameState[0] === 'x') {
                 gameActive = false;
                 winner = player1;
-                // player1Score.innerHTML++;
+                player1Score.innerHTML++;
                 status.innerHTML = `Player 1 wins!`;
             } else if (gameState[0] === 'o') {
                 gameActive = false;
                 winner = player2;
-                // player2Score.innerHTML++;
+                player2Score.innerHTML++;
                 status.innerHTML = `Player 2 wins!`
             } 
             
@@ -73,84 +73,84 @@ function checkWin () {
             if (gameState[3] === 'x') {
                 gameActive = false;
                 winner = player1;
-                // player1Score.innerHTML++;
+                player1Score.innerHTML++;
                 status.innerHTML = `Player 1 wins!`;
             } else if (gameState[3] === 'o') {
                 gameActive = false;
                 winner = player2;
-                // player2Score.innerHTML++;
+                player2Score.innerHTML++;
                 status.innerHTML = `Player 2 wins!`
             } 
         } else if (gameState[6] && gameState[7] === gameState[6] && gameState[8] === gameState[6]) { //bottom row winner
             if (gameState[6] === 'x') {
                 gameActive = false;
                 winner = player1;
-                // player1Score.innerHTML++;
+                player1Score.innerHTML++;
                 status.innerHTML = `Player 1 wins!`;
             } else if (gameState[6] === 'o') {
                 gameActive = false;
                 winner = player2;
-                // player2Score.innerHTML++;
+                player2Score.innerHTML++;
                 status.innerHTML = `Player 2 wins!`
             } 
         } else if (gameState[0] && gameState[3] === gameState[0] && gameState[6] === gameState[0]) { //left column winner
             if (gameState[0] === 'x') {
                 gameActive = false;
                 winner = player1;
-                // player1Score.innerHTML++;
+                player1Score.innerHTML++;
                 status.innerHTML = `Player 1 wins!`;
             } else if (gameState[0] === 'o') {
                 gameActive = false;
                 winner = player2;
-                // player2Score.innerHTML++;
+                player2Score.innerHTML++;
                 status.innerHTML = `Player 2 wins!`
             } 
         } else if (gameState[1] && gameState[4] === gameState[1] && gameState[7] === gameState[1]) { //middle column winner
             if (gameState[1] === 'x') {
                 gameActive = false;
                 winner = player1;
-                // player1Score.innerHTML++;
+                player1Score.innerHTML++;
                 status.innerHTML = `Player 1 wins!`;
             } else if (gameState[1] === 'o') {
                 gameActive = false;
                 winner = player2;
-                // player2Score.innerHTML++;
+                player2Score.innerHTML++;
                 status.innerHTML = `Player 2 wins!`
             } 
         } else if (gameState[2] && gameState[5] === gameState[2] && gameState[8] === gameState[2]) { //right column winner
             if (gameState[2] === 'x') {
                 gameActive = false;
                 winner = player1;
-                // player1Score.innerHTML++;
+                player1Score.innerHTML++;
                 status.innerHTML = `Player 1 wins!`;
             } else if (gameState[2] === 'o') {
                 gameActive = false;
                 winner = player2;
-                // player2Score.innerHTML++;
+                player2Score.innerHTML++;
                 status.innerHTML = `Player 2 wins!`
             } 
         } else if (gameState[0] && gameState[4] === gameState[0] && gameState[8] === gameState[0]) { //top left to bottom right winner
             if (gameState[0] === 'x') {
                 gameActive = false;
                 winner = player1;
-                // player1Score.innerHTML++;
+                player1Score.innerHTML++;
                 status.innerHTML = `Player 1 wins!`;
             } else if (gameState[0] === 'o') {
                 gameActive = false;
                 winner = player2;
-                // player2Score.innerHTML++;
+                player2Score.innerHTML++;
                 status.innerHTML = `Player 2 wins!`
             } 
         } else if (gameState[2] && gameState[4] === gameState[2] && gameState[6] === gameState[2]) { //top right to bottom left winner
             if (gameState[2] === 'x') {
                 gameActive = false;
                 winner = player1;
-                // player1Score.innerHTML++;
+                player1Score.innerHTML++;
                 status.innerHTML = `Player 1 wins!`;
             } else if (gameState[2] === 'o') {
                 gameActive = false;
                 winner = player2;
-                // player2Score.innerHTML++;
+                player2Score.innerHTML++;
                 status.innerHTML = `Player 2 wins!`
             } 
         } else if (gameState[0] && gameState[1] && gameState[2] && gameState[3] && gameState[4] && gameState[5] && gameState[6] && gameState[7] && gameState[8]) { //tie game
@@ -160,28 +160,23 @@ function checkWin () {
     // }
 }
 
+
 //iterate over each cell to make board playable
 for (let i = 0; i < cells.length; i++) {
 
-    cells[i].addEventListener('click', (e) => {
-        const classList = e.target.classList;
-        const location = classList[1];
+    cells[i].addEventListener('click', () => {
+
         if (gameActive === true && cells[i].innerHTML == ""){
             if (turns%2 === 0){ //what happens when it's player 1's turn
-                // cells[i].innerHTML = player1;
-
-                classList.add('x')
-                gameState[i] = player1;
-                
+                cells[i].innerHTML = player1;
+                gameState[i] = player1;               
                 console.log(gameState);
                 turns++;
                 status.innerHTML = `Player 2's turn`; //update status before start of player 2's turn
                 checkWin();
             } else { //what happens when it's not player 1's turn
-                // cells[i].innerHTML = player2;
-                classList.add('o')
+                cells[i].innerHTML = player2;
                 gameState[i] = player2;
-
                 console.log(gameState);
                 turns++;
                 status.innerHTML = `Player 1's turn`; //update turn status before start of player 1's turn
